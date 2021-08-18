32d047b1b9d0cad039aa0ee5f84996772bb71f98
vudeshmukh14 <vudeshmukh14@gmail.com>
2021-08-18 22:53:00 +0530

    changed f string formatting to positional arguments formatting as it gave error on gdb online compiler

--- tut01/tut01.py
+++ tut01/tut01.py
@@ -6,7 +6,7 @@ def meraki_helper(n):
     for num in n:
         st = str(num)
         if len(st)==1:
-            print(f"Yes - {st} is a Meraki number")
+            print("Yes - {0} is a Meraki number".format(st))
             cnt_meraki += 1
 
         else:
@@ -14,17 +14,17 @@ def meraki_helper(n):
             flag = True
             for ch in st[1:]:
                 if (abs(int(ch)-int(last_ch))!=1):
-                    print(f"No - {st} is not a Meraki number")
+                    print("No - {0} is not a Meraki number".format(st))
                     cnt_non_meraki += 1
                     flag = False
                     break
                 else:
                     last_ch = ch
             if flag:
-                print(f"Yes - {st} is a Meraki number")
+                print("Yes - {0} is a Meraki number".format(st))
                 cnt_meraki += 1
 
-    print(f"the input list contains {cnt_meraki} meraki and {cnt_non_meraki} non meraki numbers")
+    print("the input list contains {0} meraki and {1} non meraki numbers".format(cnt_meraki, cnt_non_meraki))
 
 
 input = [12, 14, 56, 78, 98, 54, 678, 134, 789, 0, 7, 5, 123, 45, 76345, 987654321]
