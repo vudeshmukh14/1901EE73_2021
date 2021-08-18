0afd2c64169d08dad4c989f6972f31fe2340026e
vudeshmukh14 <vudeshmukh14@gmail.com>
2021-08-18 19:17:41 +0530

    completed tut01.py - Meraki numbers

--- tut01/tut01.py
+++ tut01/tut01.py
@@ -1,1 +1,31 @@
-print("My name is Vishwajeet Deshmukh and my roll number is 1901EE73")
+def meraki_helper(n):
+
+    cnt_meraki = 0
+    cnt_non_meraki = 0
+
+    for num in n:
+        st = str(num)
+        if len(st)==1:
+            print(f"Yes - {st} is a Meraki number")
+            cnt_meraki += 1
+
+        else:
+            last_ch = st[0]
+            flag = True
+            for ch in st[1:]:
+                if (abs(int(ch)-int(last_ch))!=1):
+                    print(f"No - {st} is not a Meraki number")
+                    cnt_non_meraki += 1
+                    flag = False
+                    break
+                else:
+                    last_ch = ch
+            if flag:
+                print(f"Yes - {st} is a Meraki number")
+                cnt_meraki += 1
+
+    print(f"the input list contains {cnt_meraki} meraki and {cnt_non_meraki} non meraki numbers")
+
+
+input = [12, 14, 56, 78, 98, 54, 678, 134, 789, 0, 7, 5, 123, 45, 76345, 987654321]
+meraki_helper(input)
